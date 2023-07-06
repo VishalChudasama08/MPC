42533025256cc195bd489315f7ef9b7d958c20f2
VishalChudasama08 <chudasamavishal183@gmail.com>
2023-07-06 20:29:51 +0530

    variable type global and local

---
+++ C++_WCH/004.cpp
@@ -0,0 +1,13 @@
+//variable type global and local
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5, b = 8;
+    float pi = 3.14;
+    char v = 'V'; //save only one character if ensert more charecter then values is last character
+    cout << "This is Tutorial 004.cpp";
+    cout << "\nHere Intiger Value of a is " << a << " And Value of b is " << b;
+    cout << "\nFloting Point Value of pi is " << pi;
+    cout << "\nCharacter Value of v is " << v;
+    return 0;
+}

43a81ad57c0201bf20f5e8ac05dc1895e5d8f896
VishalChudasama08 <chudasamavishal183@gmail.com>
2023-10-05 21:07:03 +0530

    private,public,protected mode single inharitance

--- C++_WCH/038_private_and_public_and_protected_mode_on_single_inharitance.cpp
+++ C++_WCH/038_private_and_public_and_protected_mode_on_single_inharitance.cpp
@@ -52,6 +52,7 @@ int main()
 } */
 
 // single inharitance and using visibility mode is Private
+/*
 class Base
 {
     int data1; // default private variable and not inharitable this.
@@ -98,6 +99,72 @@ int main()
 {
     Derived der;
     // der.setData(); //setData() in derived class private member becose base class visibility is private, then not posible to using derived class object to calling
+    der.Prosess();
+    der.Display();
+    return 0;
+}
+*/
+
+// single inharitance and using visibility mode is Protected
+class Base
+{
+private:
+    int data1; // default private variable and not inharitable this.
+
+protected:
+    int data4; // protected member all property is sem as private, but it is member is posible to inharitance
+
+public:
+    int data2;
+    void setData();
+    int getData1();
+    int getData2();
+    int getData4();
+};
+void Base ::setData()
+{
+    data1 = 8;
+    data2 = 7;
+    data4 = 9;
+}
+int Base ::getData1()
+{
+    return data1;
+}
+int Base ::getData2()
+{
+    return data2;
+}
+int Base ::getData4()
+{
+    return data4;
+}
+
+class Derived : protected Base
+{
+    int data3;
+
+public:
+    void Prosess();
+    void Display();
+};
+void Derived ::Prosess()
+{
+    setData();
+    data3 = data4 * data2 * getData1(); // data1 not directy acsses but data4 is acsses
+}
+void Derived ::Display()
+{
+    cout << "Value Of Data 1 is " << getData1() << endl;
+    cout << "Value Of Data 2 is " << data2 << endl;
+    cout << "Value Of Data 4 is " << data4 << endl;
+    cout << "Value Of Data 3 is " << data3 << endl;
+    cout << "Defrent on line number 154";
+}
+int main()
+{
+    Derived der;
+    // der.setData(); // setData() in derived class protacted member becose base class visibility is protacted, then not posible to using derived class object to calling, becose Protacted
     der.Prosess();
     der.Display();
     return 0;

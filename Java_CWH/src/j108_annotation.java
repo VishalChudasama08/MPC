51e8e9d25b0ddc5c28c8dbad7a7b46b8f4c27703
VishalChudasama08 <chudasamavishal183@gmail.com>
2024-01-13 21:17:37 +0530

    108 annotions

---
+++ Java_CWH/src/j108_annotation.java
@@ -0,0 +1,29 @@
+@FunctionalInterface // if you make in the future, not add method in interface
+interface MyInterface {
+    void method1();
+}
+
+class OneClass{
+    public void oneMethod(){
+        System.out.println("I am OneClass oneMethod");
+    }
+}
+
+class TwoClass extends OneClass {
+    @Override // if method is override than use override annotation
+    public void oneMethod(){
+        System.out.println("I am Override TwoClass oneMethod");
+    }
+    @Deprecated // if you not use this method than make defined @Deprecated
+    public int sum(int a, int b){ // if you use this method than showing warning
+        return a+b;
+    }
+}
+public class j108_annotation {
+    // @SuppressWarnings("deprecation") // in this method you use any deprecated method than write this to run perfectly
+    public static void main(String[] args) {
+        TwoClass t = new TwoClass();
+        t.oneMethod();
+        System.out.println(t.sum(10, 20));
+    }
+}

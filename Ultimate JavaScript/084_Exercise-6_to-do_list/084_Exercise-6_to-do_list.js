7992f5c8f9edf55a53efbc962d1b152c55a220d1
VishalChudasama08 <chudasamavishal183@gmail.com>
2024-01-15 23:04:20 +0530

    exrcise-6 todo list

---
+++ Ultimate JavaScript/084_Exercise-6_to-do_list/084_Exercise-6_to-do_list.js
@@ -0,0 +1,21 @@
+submit.addEventListener("click", (e) => { // submit is submit button id
+    e.preventDefault(); // if web page reload than use this to not reloa in this case submit button
+
+    let titlec = title.value; // title is "To-Do List" input tag id
+    let descc = desc.value; // desc is "ToDo Desc" input tag id
+
+    localStorage.setItem("todo", JSON.stringify([titlec, descc])); // click submit then stor titlec and descc value in localstorage 
+    // JSON.stringify() use to convert string
+
+    console.log(e);
+
+    todo.innerHTML = `<h2>${titlec}</h2><h4>${descc}</h4>` //add todo id in html
+
+    title.value = ""; // after submit clear input bot
+    desc.value = ""; // after submit clear input bot
+})
+deletebtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    localStorage.removeItem("todo");
+    console.log(e);
+})

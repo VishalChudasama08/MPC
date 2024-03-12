f857384961c543d42eabcafa1ff1aafceb680b86
VishalChudasama08 <chudasamavishal183@gmail.com>
2024-03-12 22:43:26 +0530

    094 update

--- Ultimate JavaScript/094_exercise-8_alarm_clock/094_exercise-8_alarm_clock.js
+++ Ultimate JavaScript/094_exercise-8_alarm_clock/094_exercise-8_alarm_clock.js
@@ -28,11 +28,30 @@
 //     }
 // }
 
-function play() {
+setInterval(function () {
+    let now = new Date();
+    let date = now.getDate();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds;
+    console.log('Clock is Raning');
+}, 1000);
+
+let v = prompt("Enter Number after ringing alarm clock") * 1000;
+console.log(v);
+// if (v != 0) {
+setTimeout(function () {
     let audio = new Audio('https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3');
-    audio.play().then(() => {
+    audio.play().then(() => { // .play() start alarm sound
         console.log("playing...");
+        let shouldDismiss = confirm("Alarm is ringing! Would you like to dismiss it?");
+        if (shouldDismiss) {
+            audio.pause(); // Stop the alarm sound
+            console.log("Alarm dismissed by user.");
+        }
     }).catch(error => {
         console.error("Error playing audio:", error);
     });
-}
+}, v);
+// }

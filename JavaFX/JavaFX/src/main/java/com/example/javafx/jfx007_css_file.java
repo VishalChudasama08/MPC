ff0d1c9e24731d9f27ce3902e56f7c60c97e79bf
VishalChudasama08 <chudasamavishal183@gmail.com>
2024-04-01 21:35:32 +0530

    edit idea

---
+++ JavaFX/JavaFX/src/main/java/com/example/javafx/jfx007_css_file.java
@@ -0,0 +1,23 @@
+package com.example.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class jfx007_css_file extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("jfx007_css_file.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        String css = this.getClass().getResource("fx007_css_file.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setTitle("Add Css File");
+        stage.setScene(scene);
+        stage.show();
+    }
+}

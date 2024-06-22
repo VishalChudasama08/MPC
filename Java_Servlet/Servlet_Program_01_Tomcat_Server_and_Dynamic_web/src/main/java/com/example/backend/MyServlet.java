957a47e4870b382889e78b2fa792bebf26bf10cd
VishalChudasama08 <chudasamavishal183@gmail.com>
2024-06-23 01:53:50 +0530

    add servlet program 03 first servlet program and 04 servlet life cycle

---
+++ Java_Servlet/Servlet_Program_01_Tomcat_Server_and_Dynamic_web/src/main/java/com/example/backend/MyServlet.java
@@ -0,0 +1,19 @@
+package com.example.backend;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet; //mostly use this
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("I am in doGet() method. for browser"); // print in browser view page
+        System.out.println("I am in doGet() method."); // this print in console (not browser console)
+    }
+}

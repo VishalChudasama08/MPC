030dff4885cf4c13f1e964015f0354bbb6b1964d
VishalChudasama08 <chudasamavishal183@gmail.com>
2024-08-01 12:01:53 +0530

    add spring program 16 in java spring framework it is run perfect but image not showing

---
+++ Java_Spring_Framework/Spring_Program_16_Spring_WEB-MVC_First_Program_eclipse/src/main/java/spring/web/mvc/controller/MyController.java
@@ -0,0 +1,32 @@
+package spring.web.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController  {
+	@RequestMapping("/")
+	public String home() {
+		return "index"; // This index is `index.jsp` file nu name and return value same hovi joye
+	}
+	@RequestMapping("/index")
+	public String indexPage() {
+		return "index"; // This should match a JSP file like `index.jsp`
+	}
+	@RequestMapping(value = "/helloPage", method = RequestMethod.GET) // this annotation accept all type of request, You can set methods like get, post
+//	@GetMapping("/helloPage")
+	public ModelAndView openHelloPage() {
+		System.out.println("openHelloPage() methos exituted. it minas Opened Hello page...");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("hello"); // This should match a JSP file like `hello.jsp`
+		return mav; // Corresponds to hello.jsp
+	}
+
+	@GetMapping("/aboutUsPage")
+	public String openAboutUsPage() {
+	    return "aboutUs"; // Corresponds to aboutUs.jsp // This should match a JSP file like `hello.jsp`
+	}
+}

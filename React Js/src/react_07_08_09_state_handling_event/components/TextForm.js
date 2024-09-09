1789bd75d57f8fc7bd5b3b5e8a6789842129bc90
VishalChudasama08 <chudasamavishal183@gmail.com>
2024-09-09 12:08:26 +0530

    React update code in new R10 file and add R12 full program with its file

--- React Js/src/react_07_08_09_state_handling_event/components/TextForm.js
+++ React Js/src/react_07_08_09_state_handling_event/components/TextForm.js
@@ -2,12 +2,21 @@ import React, { useState } from 'react'
 
 const TextForm = (props) => {
     const [text, setText] = useState('Enter Text Here...')
-    // this is 'State', for use first import it 
-    // and writ in component 
+    // this is 'State', if use State than import it 
+    // use for write a component 
+    // first is variable (any name, here than is text)
+    // second is function. use for set first variable value and more (any name, here than is setText)
     // setText("new Text"); // correct way to change text
+    // in useState() argument is set variable type like in this 'text' is String type 
+    // if useState(false) than that boolean type
+    // if useState({
+    //      name: 'vishal',
+    //      age: 23
+    // }) than that is object 
+
 
     const handleOnChange = (event) => {
-        setText(event.target.value) // without this user not edit/add/remove text in textarea tag
+        setText(event.target.value) // event.target.value -> without this user not edit/add/remove text in textarea tag
     }
 
     const handleUpClick = () => {
@@ -30,6 +39,12 @@ const TextForm = (props) => {
     const handleBoClick = () => {
         mybox.classList.toggle("fw-bold"); // toggle class
     }
+    const handleCopy = () => {
+        mybox.classList.toggle("fw-bold"); // toggle class
+        let copyText = document.getElementById('myBox');
+        copyText.select();
+        navigator.clipboard.writeText(copyText.value);
+    }
 
     return (
         <>
@@ -38,6 +53,7 @@ const TextForm = (props) => {
                 <div className='mb-3'>
                     <textarea className='form-control' value={text} onChange={handleOnChange} id="myBox" rows="8" ></textarea>
                 </div>
+                <button className='btn btn-primary m-1' onClick={handleCopy}>Copy Clipboard</button>
                 <button className='btn btn-primary m-1' onClick={handleUpClick}>Upper Case</button>
                 <button className='btn btn-primary m-1' onClick={handleLoClick}>Lower Case</button>
                 <button className='btn btn-primary m-1' onClick={handleClClick}>Clear</button>

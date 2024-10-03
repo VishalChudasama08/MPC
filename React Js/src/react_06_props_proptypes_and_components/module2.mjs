42561dd70ba3c7fa2556d0e4f90ff6c7c23cfc03
Chudasama Vishal <chudasamavishal183@gmail.com>
2024-10-03 10:30:49 +0530

    delete React first created project, add old css program
    React:- react started time created project delete because project add on it's perticular folder
    CSS:- add animatin checkbox program

--- React Js/src/react_06_props_proptypes_and_components/module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Vishal";
-const b = "This is const b";
-const c = "This is const c";
-const d = "This is const d";
-
-export default a; // set default for export
-export { b };
-export { c };
-export { d };

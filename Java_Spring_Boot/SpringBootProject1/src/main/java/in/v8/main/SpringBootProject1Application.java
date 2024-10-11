bd0529f3b97f6a1d05ec21590e85d92a3c12a08e
Chudasama Vishal <chudasamavishal183@gmail.com>
2024-10-12 01:12:39 +0530

    Spring Boot add V3 SpringBootProject1first project
    From Video number #3

---
+++ Java_Spring_Boot/SpringBootProject1/src/main/java/in/v8/main/SpringBootProject1Application.java
@@ -0,0 +1,15 @@
+package in.v8.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject1Application.class, args);
+		
+		System.out.println("Hello Vishal ! This is Spring Boot First Pgoram");
+	}
+
+}

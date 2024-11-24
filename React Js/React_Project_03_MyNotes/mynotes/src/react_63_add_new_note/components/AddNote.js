0edb9b5ad75de6af7e6b668301796db481a18477
Chudasama Vishal <chudasamavishal183@gmail.com>
2024-11-24 15:35:59 +0530

    React js add V63 add new note
    create AddNote.js component, and insert in Notes.js component

---
+++ React Js/React_Project_03_MyNotes/mynotes/src/react_63_add_new_note/components/AddNote.js
@@ -0,0 +1,41 @@
+import React, { useContext, useState } from 'react'
+import noteContext from '../context/notes/NoteContext'
+
+const AddNote = () => {
+    const { addNote } = useContext(noteContext); // only need addNote useState than get this from useContext hook (present in NoteState.js file)
+
+    const [note, setNote] = useState({ title: "", description: "", tag: "" })
+
+    // handle onChange all input
+    const onChange = (event) => {
+        setNote({ ...note, [event.target.name]: event.target.value })
+    }
+
+    // handle submit
+    const handleSubmitNote = (event) => {
+        event.preventDefault()
+        addNote(note)
+    }
+    return (
+        <div>
+            <h2>Add a Note</h2>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label">Make Title</label>
+                    <input type="text" name='title' onChange={onChange} className="form-control" id="title" placeholder="what is note title ?" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="description" className="form-label">Enter New Note</label>
+                    <textarea name='description' onChange={onChange} className="form-control" id="description" placeholder='what is note description ?' rows="3"></textarea>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="tag" className="form-label">Make Tags</label>
+                    <input type="text" name='tag' onChange={onChange} className="form-control" id="tag" placeholder="what is note tag ?" />
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={handleSubmitNote}>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddNote

7c889ff9da97dc5027e5eb60b4e2821fa3accf14
Chudasama Vishal <chudasamavishal183@gmail.com>
2024-12-15 01:48:13 +0530

    etide same comments

--- React Js/Redux/src/components/CounterMiter.js
+++ React Js/Redux/src/components/CounterMiter.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const CounterMiter = () => {
-    const count = useSelector((state) => state.counter.count);
+    const count = useSelector((state) => state.counter.count); // get counter count data from redux store
+    // state.counter.count => 
+    // 'state'->redux store state, 
+    // 'counter'->reducer: { counter: counterReducer,... } in 'store' function on store.js file,
+    // 'count'-> counter state name from here counterReducer.js file 
 
     return (
         <button disabled className='btn btn-outline-info mx-3'>Counter Miter: {count}</button>

452243ba077c666369da3e2c27824bd1a0cca65a
Chudasama Vishal <chudasamavishal183@gmail.com>
2025-01-02 19:38:35 +0530

    add keep note project as spring web mvc with maven
    not working

---
+++ Java_Spring_Framework/Keep_Notes/src/main/java/in/v8/main/controllers/MyController.java
@@ -0,0 +1,24 @@
+package in.v8.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController  {
+	@RequestMapping(value = "/helloPage", method = RequestMethod.GET) // this annotation accept all type of request, You can set methods like get, post
+//	@GetMapping("/helloPage") // same as in index.jsp file present "Open hello page" link href attribute value:- href="helloPage"
+	public ModelAndView openHelloPage() {
+		System.out.println("openHelloPage() methos exituted. it minas Opened Hello page...");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("hello"); // This should match a JSP file like `hello.jsp`
+		return mav; // Corresponds to hello.jsp
+	}
+
+	@GetMapping("/aboutUsPage") // same as in index.jsp file present "About Us" link href attribute value:- href="aboutUsPage"
+	public String openAboutUsPage() {
+	    return "aboutUs"; // Corresponds to aboutUs.jsp // This should match a JSP file like `aboutUs.jsp`
+	}
+}

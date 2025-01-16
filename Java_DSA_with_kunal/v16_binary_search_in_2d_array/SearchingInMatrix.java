a1afdd71d13d7a838941a926e106425b39fc9ca8
Chudasama Vishal <chudasamavishal183@gmail.com>
2025-01-16 22:48:21 +0530

    v16 binary search in 2d array
    Searching in matrix

---
+++ Java_DSA_with_kunal/v16_binary_search_in_2d_array/SearchingInMatrix.java
@@ -0,0 +1,34 @@
+package v16_binary_search_in_2d_array;
+
+import java.util.Arrays;
+
+public class SearchingInMatrix {
+    public static void main(String[] args) {
+        int [][] arr = {
+            {10, 20, 30, 40, 50},
+            {12, 22, 32, 42, 52},
+            {14, 24, 34, 44, 54},
+            {16, 26, 36, 46, 56},
+            {18, 28, 38, 48, 58},
+        };
+        int target = 18;
+
+        System.out.println(Arrays.toString(searchingInMatrix(arr, target)));
+    }
+    public static int[] searchingInMatrix(int[][] matrix, int target){
+        int row = 0;
+        int column = matrix.length - 1; // it's return max row but in matrix both are same than max column is matrix.length-1
+
+        while (row < matrix.length && column >= 0){
+            if(matrix[row][column] == target){
+                return new int[]{row, column};
+            }
+            if(matrix[row][column] < target){
+                row++;
+            } else {
+                column--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
---
+++ Java_Spring_Boot/SpringBootProject8_JDBC_MySql_CRUD/src/main/resources/spring_boot_jdbc.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 16, 2025 at 01:04 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spring_boot_jdbc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table spring_boot_jdbc.users: #1932 - Table &#039;spring_boot_jdbc.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table spring_boot_jdbc.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `spring_boot_jdbc`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
